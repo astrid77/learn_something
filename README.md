f4ebc51687f7ad6dd12ac533f92dbcd1122998de
wzx <178750871@q.com>
2025-03-26 06:50:18 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # learn_something
+## Python
+### 装饰器练习题
+
+- 实现一个装饰器，打印函数的输入参数和返回值。
+代码实现：[print_input_output.py](https://github.com/astrid77/learn_something/about_python/print_input_output.py)
+- 实现一个装饰器，限制函数每秒最多调用1次（简单版限流）。
+代码实现：[limit_request_freq.py](https://github.com/astrid77/learn_something/about_python/limit_request_freq.py)
---
+++ about_python/print_input_output.py
@@ -0,0 +1,28 @@
+# 1、实现一个装饰器，打印函数的输入参数和返回值
+import functools
+
+
+def print_input_output(func):
+    """打印输入输出的装饰器"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        input_list = []
+        input_list.extend([f"{x}" for x in args])
+        input_list.extend(f"{x}={y}" for x, y in kwargs.items())
+
+        input_to_str = ",".join(input_list)
+        print(f"{func.__name__}函数的输入参数为：({input_to_str})")
+        output = func(*args, **kwargs)
+        print(f"{func.__name__}函数的返回值为：{output}")
+        return output
+
+    return wrapper
+
+
+@print_input_output
+def add(x, y, a=1, b=2):
+    return x + y + a + b
+
+
+add(11, 22)
+add(1, 2, a=3, b=4)
